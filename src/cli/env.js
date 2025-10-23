3e4f67c0205f862a8f3200f2b10badf19e1d0600
Нина Евлаш <evlash.nina@mail.ru>
2025-10-23 13:44:52 +0200

    feat: implement tasks from the cli  folder

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-  // Write your code here
+  const envObj = process.env;
+  const arr = Object.entries(envObj).filter(([key]) => key.startsWith('RSS_'));
+  const arrString = arr.map(([key, value]) => `${key} = ${value}`);
+  const result = arrString.join('; ')
+  console.log(result);
 };
 
 parseEnv();
