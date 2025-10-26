a5928c95340846652c5cf530306c5918c066a300
Нина Евлаш <evlash.nina@mail.ru>
2025-10-26 16:14:57 +0100

    feat: add code Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const filePath = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [filePath, args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('one', 'two', 'three');
